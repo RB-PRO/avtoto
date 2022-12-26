e7915e42be9420438ca6ba67453ba59b0eb87884
Roman Blinov <romanblinov2013@yandex.ru>
2022-12-26 23:16:55 +0300

    Add try SearchStart

--- SearchStart.go
+++ SearchStart.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 )
 
@@ -19,41 +21,92 @@ import (
 	}
 */
 
-type SearchStartRequest struct {
-	user_id       int    `json:"user_id"`       // Уникальный идентификатор пользователя (номер клиента) (тип: целое)
-	user_login    string `json:"user_login"`    // Логин пользователя (тип: строка)
-	user_password string `json:"user_password"` // Пароль пользователя (тип: строка)
-	search_code   string `json:"search_code"`   // Поисковый запрос, минимум 3 символа (тип: строка)
-	search_cross  string `json:"search_cross"`  // Искать в аналогах или нет (тип: строка, 'on' или 'off')
-	brand         string `json:"brand"`         /// Искать код с учетом бренда, минимум 2 символа (опционально)(тип: строка)
+type SearchStartRequestStruct struct {
+	UserId       int    `json:"user_id"`       // Уникальный идентификатор пользователя (номер клиента) (тип: целое)
+	UserLogin    string `json:"user_login"`    // Логин пользователя (тип: строка)
+	UserPassword string `json:"user_password"` // Пароль пользователя (тип: строка)
+	SearchCode   string `json:"search_code"`   // Поисковый запрос, минимум 3 символа (тип: строка)
+	SearchCross  string `json:"search_cross"`  // Искать в аналогах или нет (тип: строка, 'on' или 'off')
+	Brand        string `json:"brand"`         /// Искать код с учетом бренда, минимум 2 символа (опционально)(тип: строка)
 	// [*] — эти данные можно узнать зайдя на страницу Настройки после авторизации на сайте
 	// [**] Список брендов можно получить с помощью метода GetBrandsByCode
 }
-
-type searchStartRequestData struct {
-	action string             `json:"action"`
-	data   SearchStartRequest `json:"data"`
+type SearchStartResponseStruct struct {
+	ProcessSearchID string `json:"ProcessSearchId"`
+	Info            struct {
+		SearchID string   `json:"SearchId"`
+		Errors   []string `json:"Errors"`
+		Logs     string   `json:"Logs"`
+	} `json:"Info"`
 }
 
-func (searchReq SearchStartRequest) Post() {
-	RequestData := searchStartRequestData{action: "SearchStart", data: searchReq}
+func (user User) SearchStartRequest(searchStartReq SearchStartRequestStruct) (SearchStartResponseStruct, error) {
+	searchStartReq.UserId = user.UserId
+	searchStartReq.UserLogin = user.UserLogin
+	searchStartReq.UserPassword = user.UserPassword
 
-	personJSON, _ := json.Marshal(RequestData)
+	// Ответ от сервера
+	var responseSearchStart SearchStartResponseStruct
 
-	response, error := http.Post(URL,
-		"data", bytes.NewBuffer(personJSON))
-
-	if error != nil {
-		print(error)
+	// Подготовить данные для загрузки
+	bytesRepresentation, responseError := json.Marshal(searchStartReq)
+	if responseError != nil {
+		return responseSearchStart, responseError
 	}
 
-	defer response.Body.Close()
-	body, errorasd := ioutil.ReadAll(response.Body)
-	if errorasd != nil {
-		print(errorasd)
+	// Отправить данные
+	body, responseError := httpPost(bytesRepresentation, "SearchStart")
+	if responseError != nil {
+		return responseSearchStart, responseError
 	}
 
-	fmt.Println(string(body))
+	// Распарсить данные
+	responseError = responseSearchStart.UnmarshalJson(body)
+
+	return responseSearchStart, responseError
+}
+
+func (responseSearchStart *SearchStartResponseStruct) UnmarshalJson(body []byte) error {
+	responseError := json.Unmarshal(body, &responseSearchStart)
+	if responseError != nil {
+		return responseError
+	}
+	fmt.Println("ERROR", responseSearchStart.Info.Errors)
+	if len(responseSearchStart.Info.Errors) != 0 {
+		return errors.New(responseSearchStart.Info.Errors[0])
+	}
+	return nil
+}
+
+func httpPost(bytesRepresentation []byte, action string) ([]byte, error) {
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("action", "SearchStart")
+	_ = writer.WriteField("data", string(bytesRepresentation))
+	responseError := writer.Close()
+	if responseError != nil {
+		return nil, responseError
+	}
+
+	client := &http.Client{}
+	req, responseError := http.NewRequest(http.MethodPost, URL, payload)
+	if responseError != nil {
+		return nil, responseError
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, responseError := client.Do(req)
+	if responseError != nil {
+		return nil, responseError
+	}
+	defer res.Body.Close()
+
+	// Считываем ответ
+	body, responseError := ioutil.ReadAll(res.Body)
+	if responseError != nil {
+		return nil, responseError
+	}
+	return body, responseError
 }
 
 /*
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
+type User struct {
+	UserId       int    `json:"user_id"`       // Уникальный идентификатор пользователя (номер клиента) (тип: целое)
+	UserLogin    string `json:"user_login"`    // Логин пользователя (тип: строка)
+	UserPassword string `json:"user_password"` // Пароль пользователя (тип: строка)
+}
+
+const URL string = "https://www.avtoto.ru/?soap_server=json_mode"
+
+func main() {
+	//sreq := SearchStartRequestStruct{user_id: 532936, user_login: "s532936", user_password: "123456z", search_code: "N007603010406", search_cross: "off", brand: "MERCEDES-BENZ"}
+	//sreq.Post()
+	user := User{UserId: 532936, UserLogin: "s532936", UserPassword: "123456z"}
+	searchStartReq := SearchStartRequestStruct{SearchCode: "N007603010406", SearchCross: "on", Brand: "MERCEDES-BENZ"}
+	jsons, errorSearch := user.SearchStartRequest(searchStartReq)
+	if errorSearch != nil {
+		log.Fatal(errorSearch)
+	}
+	fmt.Println(jsons)
+}
+
 /*case 'cannot create client': return 'Не получилось соединиться с сервером';
   case 'no result':            return 'Сервер не ответил';
   case 'wrong params':         return 'Неверные параметры соединения';
@@ -14,11 +39,5 @@ package main
       '6'=>  'На складе',
       '7'=>  'Выдано',
       '8'=>  'Нет в наличии'
-  );	*/
-
-const URL string = "https://www.avtoto.ru/?soap_server=json_mode"
-
-func main() {
-	sreq := SearchStartRequest{user_id: 532936, user_login: "s532936", user_password: "123456z", search_code: "N007603010406", search_cross: "off", brand: "MERCEDES-BENZ"}
-	sreq.Post()
-}
+  );
+*/
