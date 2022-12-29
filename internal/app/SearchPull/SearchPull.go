52ff2094eded193d394caba0e7dea08360f198b6
Roman Blinov <romanblinov2013@yandex.ru>
2022-12-29 17:29:53 +0300

    workn'tgit add .!

--- internal/app/SearchPull/SearchPull.go
+++ internal/app/SearchPull/SearchPull.go
@@ -1,7 +1,64 @@
 package searchpull
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
 
-func prints() {
+	"github.com/rb-pro/avtoto/pkg/avtotoGo"
+)
+
+func Start() {
 	fmt.Println("SearchPull")
+
+	userIdInt, _ := strconv.Atoi(dataFile("UserId.txt"))
+	UserLoginStr := dataFile("UserLogin.txt")
+	UserPasswordStr := dataFile("UserPassword.txt")
+
+	user := avtotoGo.User{UserId: userIdInt, UserLogin: UserLoginStr, UserPassword: UserPasswordStr}
+
+	searchStartReq := avtotoGo.SearchStartRequestStruct{SearchCode: "N007603010406", SearchCross: "on", Brand: "MERCEDES-BENZ"}
+	jsons, errorSearch := user.SearchStartRequest(searchStartReq)
+	if errorSearch != nil {
+		log.Fatal(errorSearch)
+	}
+	fmt.Println(jsons)
 }
+
+func dataFile(filename string) string {
+	fileToken, errorToken := os.Open(filename)
+	if errorToken != nil {
+		log.Fatal(errorToken)
+	}
+	defer func() {
+		if errorToken = fileToken.Close(); errorToken != nil {
+			log.Fatal(errorToken)
+		}
+	}()
+	data, errFileToken := ioutil.ReadAll(fileToken)
+	if errFileToken != nil {
+		log.Fatal(errFileToken)
+	}
+	return string(data)
+}
+
+/*
+case 'cannot create client': return 'Не получилось соединиться с сервером';
+
+	case 'no result':            return 'Сервер не ответил';
+	case 'wrong params':         return 'Неверные параметры соединения';
+	case 'wrong parts':          return 'Ошибка данных';
+	case 'error code':           return 'Неверный артикул';
+	private $progress_list = array(
+	    '2'=>  'Ожидает оплаты',
+	    '1'=>  'Ожидает обработки',
+	    '3'=>  'Заказано',
+	    '4'=>  'Закуплено',
+	    '5'=>  'В пути',
+	    '6'=>  'На складе',
+	    '7'=>  'Выдано',
+	    '8'=>  'Нет в наличии'
+	);
+*/
--- pkg/avtotoGo/httpCustom.go
+++ pkg/avtotoGo/httpCustom.go
@@ -7,6 +7,15 @@ import (
 	"net/http"
 )
 
+const URL string = "https://www.avtoto.ru/?soap_server=json_mode"
+
+// Исходная структура для авторизации пользователя
+type User struct {
+	UserId       int    `json:"user_id"`       // Уникальный идентификатор пользователя (номер клиента) (тип: целое)
+	UserLogin    string `json:"user_login"`    // Логин пользователя (тип: строка)
+	UserPassword string `json:"user_password"` // Пароль пользователя (тип: строка)
+}
+
 // Запрос с параметром action и данными json в формате []byte
 func HttpPost(bytesRepresentation []byte, action string) ([]byte, error) {
 	payload := &bytes.Buffer{}
